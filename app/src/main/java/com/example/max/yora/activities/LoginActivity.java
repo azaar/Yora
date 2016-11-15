ec633fa910f533182646f20aab7fa5951c94100a
Max <rustycrusty01@gmail.com>
2016-11-15 15:56:30 +0300

    app 004 - work on login

--- app/src/main/java/com/example/max/yora/activities/LoginActivity.java
+++ app/src/main/java/com/example/max/yora/activities/LoginActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.max.yora.R;
+import com.example.max.yora.fragments.LoginFragment;
 
-public class LoginActivity extends BaseActivity implements View.OnClickListener {
+public class LoginActivity extends BaseActivity implements View.OnClickListener, LoginFragment.Callbacks{
+    private static final int REQUEST_NARROW_LOGIN = 1;
+
     private View loginButton;
+
+
     @Override
     protected void onCreate(Bundle savedState) {
         super.onCreate(savedState);
@@ -23,7 +28,29 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
     @Override
     public void onClick(View view) {
         if (view == loginButton) {
-            startActivity(new Intent(this, LoginNarrowActivity.class));
+            startActivityForResult(new Intent(this, LoginNarrowActivity.class), REQUEST_NARROW_LOGIN);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_NARROW_LOGIN) {
+            finishLogin();
+        }
+    }
+
+    private void finishLogin() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onLoggedIn() {
+        finishLogin();
     }
 }
--- app/src/main/java/com/example/max/yora/activities/LoginNarrowActivity.java
+++ app/src/main/java/com/example/max/yora/activities/LoginNarrowActivity.java
@@ -4,12 +4,19 @@ package com.example.max.yora.activities;
 import android.os.Bundle;
 
 import com.example.max.yora.R;
+import com.example.max.yora.fragments.LoginFragment;
 
-public class LoginNarrowActivity extends BaseActivity {
+public class LoginNarrowActivity extends BaseActivity implements LoginFragment.Callbacks {
     @Override
     protected void onCreate(Bundle savedState) {
         super.onCreate(savedState);
 
         setContentView(R.layout.activity_login_narrow);
+    }
+
+    @Override
+    public void onLoggedIn() {
+        setResult(RESULT_OK);
+        finish();
     }
 }
