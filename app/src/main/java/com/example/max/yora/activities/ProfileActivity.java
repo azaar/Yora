3f7222e1247cdbd5bd804cd3fd7abe302abf350d
Max <rustycrusty01@gmail.com>
2016-11-20 13:27:02 +0300

    app 012 - profile layout

--- app/src/main/java/com/example/max/yora/activities/ProfileActivity.java
+++ app/src/main/java/com/example/max/yora/activities/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.max.yora.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.max.yora.R;
 import com.example.max.yora.views.MainNavDrawer;
@@ -10,5 +12,16 @@ public class ProfileActivity extends BaseAuthenticatedActivity {
     protected void onYoraCreate(Bundle savedState) {
         setContentView(R.layout.activity_profile);
         setNavDrawer(new MainNavDrawer(this));
+
+        if (!isTablet) {
+            View textFields = findViewById(R.id.activity_profile_textFields);
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) textFields.getLayoutParams();
+            params.setMargins(0, params.getMarginStart(), 0, 0);
+            params.removeRule(RelativeLayout.END_OF);
+            params.addRule(RelativeLayout.BELOW, R.id.activity_profile_changeAvatar);
+            textFields.setLayoutParams(params);
+
+        }
     }
 }
