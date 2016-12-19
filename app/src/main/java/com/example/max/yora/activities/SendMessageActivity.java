b035aced7c08b943d140a8946438242724ec1a8b
Max <rustycrusty01@gmail.com>
2016-12-19 23:39:40 +0300

    app 040 - send message layout

--- app/src/main/java/com/example/max/yora/activities/SendMessageActivity.java
+++ app/src/main/java/com/example/max/yora/activities/SendMessageActivity.java
@@ -1,22 +1,50 @@
 package com.example.max.yora.activities;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.max.yora.R;
+import com.example.max.yora.services.Messages;
+import com.example.max.yora.services.entities.UserDetails;
 import com.squareup.picasso.Picasso;
 
-public class SendMessageActivity extends BaseAuthenticatedActivity {
+public class SendMessageActivity extends BaseAuthenticatedActivity implements View.OnClickListener {
     public static final String EXTRA_IMAGE_PATH = "EXTRA_IMAGE_PATH";
     public static final String EXTRA_CONTACT = "EXTRA_CONTACT";
 
     public static final int MAX_IMAGE_HEIGHT = 1280;
 
+    private static final String STATE_REQUEST = "STATE_REQUEST";
+    private static final int REQUEST_SELECT_RECIPIENT = 1;
+
+    private Messages.SendMessageRequest request;
+    private EditText messageEditText;
+    private Button recipientButton;
+    private View progressFrame;
+
 
     @Override
     protected void onYoraCreate(Bundle savedState) {
         setContentView(R.layout.activity_send_message);
+        getSupportActionBar().setTitle(R.string.send_message);
+
+        // TODO: Check if it's a bad practice. If so, refactor.
+        if (savedState != null) {
+            request = savedState.getParcelable(STATE_REQUEST);
+        }
+
+        if (request == null) {
+            request = new Messages.SendMessageRequest();
+            request.setRecipient((UserDetails) getIntent().getParcelableExtra(EXTRA_CONTACT));
+        }
 
         Uri imageUri = getIntent().getParcelableExtra(EXTRA_IMAGE_PATH);
 
@@ -25,5 +53,58 @@ public class SendMessageActivity extends BaseAuthenticatedActivity {
             Picasso.with(this).load(imageUri).into(image);
         }
 
+        if (getResources().getConfiguration().orientation != Configuration.ORIENTATION_PORTRAIT) {
+            View optionsFrame = findViewById(R.id.activity_send_message_optionsFrame);
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) optionsFrame.getLayoutParams();
+
+            params.addRule(RelativeLayout.ALIGN_PARENT_END);
+            params.addRule(RelativeLayout.BELOW, R.id.include_toolbar);
+
+            params.width = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    300,
+                    getResources().getDisplayMetrics());
+
+            optionsFrame.setLayoutParams(params);
+        }
+
+        progressFrame = findViewById(R.id.activity_send_message_progressFrame);
+        recipientButton = (Button) findViewById(R.id.activity_send_message_recipientButton);
+        messageEditText = (EditText) findViewById(R.id.activity_send_message_message);
+
+        progressFrame.setVisibility(View.GONE);
+
+        recipientButton.setOnClickListener(this);
+        updateButtons();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == recipientButton) {
+            selectRecipient();
+        }
+    }
+
+    private void updateButtons() {
+        UserDetails recipient = request.getRecipient();
+        if (recipient != null) {
+            recipientButton.setText(getResources().getText(R.string.to_colon) + recipient.getDisplayName());
+        } else {
+            recipientButton.setTag(getResources().getText(R.string.choose_recipient));
+        }
+    }
+
+    private void selectRecipient() {
+        startActivityForResult(new Intent(this, SelectContactActivity.class), REQUEST_SELECT_RECIPIENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SELECT_RECIPIENT && resultCode == RESULT_OK) {
+            UserDetails selectedContact = data.getParcelableExtra(SelectContactActivity.RESULT_CONTACT);
+            request.setRecipient(selectedContact);
+            updateButtons();
+        }
     }
 }
